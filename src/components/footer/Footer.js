00f125f3c9a2faeda44fa750ecb68d40e30e7dbc
Mostafa <sm.mostafajamal@gmail.com>
2023-02-27 12:17:40 +0600

    completed contact us page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,7 @@ import {
   FaLinkedin,
   FaMailBulk,
 } from "react-icons/fa";
+import Flag from "react-world-flags";
 import { BsTelephoneFill } from "react-icons/bs";
 import "./footer.css";
 import { Link } from "react-router-dom";
@@ -12,80 +13,109 @@ const Footer = () => {
     <div className="footer-container">
       <div className="footerWrapper">
         <div className="about">
-          <div className="steadfastLogo-container">
-            <img
-              src={`${process.env.PUBLIC_URL}/steadfast_images/steadfast-logo-withoutTxt.png`}
-              alt="steadfast communication logo"
-              className="steadfast-logo"
-            />
-            <h3>STEADFAST COMMUNICATIONS</h3>
+          <div className="aDetails">
+            <div className="steadfastLogo-container">
+              <img
+                src={`${process.env.PUBLIC_URL}/steadfast_images/steadfast-logo-withoutTxt.png`}
+                alt="steadfast communication logo"
+                className="steadfast-logo"
+              />
+              <h3>STEADFAST COMMUNICATIONS</h3>
+            </div>
+            <article>
+              Steadfast International Services is a technology-based recruitment
+              organization. It is a principal of technology consulting and
+              workforce management solutions Company, with city of heart offices
+              in the New York, United States and Dhaka, Bangladesh. It provides
+              recruitment services for Technology, Information Technology and
+              Services (IT), Engineering, Technician, Finance, Administrative
+              Professional, Telecommunications, Pharmaceutical and Life
+              Sciences, Chemicals, Energy, Oil and Gas, Healthcare,
+              Professionals and many more!
+            </article>
           </div>
-          <article>
-            Steadfast International Services is a technology-based recruitment
-            organization. It is a principal of technology consulting and
-            workforce management solutions Company, with city of heart offices
-            in the New York, United States and Dhaka, Bangladesh. It provides
-            recruitment services for Technology, Information Technology and
-            Services (IT), Engineering, Technician, Finance, Administrative
-            Professional, Telecommunications, Pharmaceutical and Life Sciences,
-            Chemicals, Energy, Oil and Gas, Healthcare, Professionals and many
-            more!
-          </article>
+          <div className="address">
+            <h3>Office Addresses</h3>
+            <div>
+              <Flag code="us" height="20" className="flagIcon" />
+              <span>
+                733 3rd Ave, Floor 16 #1029, New York, NY 10017, United States
+              </span>
+            </div>
+            <div>
+              <Flag code="bd" height="22" className="flagIcon" />
+              <span>
+                Dhaka Uddan, Adabor, Mohammadpur, Dhaka 1207, Bangladesh
+              </span>
+            </div>
+          </div>
         </div>
         <div className="openningHours">
           <h3>Opening Hours</h3>
           <ul>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>Monday-Friday: 8am-6pm</span>
+              <span>
+                <strong>Monday-Friday:</strong> 8am-6pm
+              </span>
             </li>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>Saturday-Sunday: Closed</span>
+              <span>
+                <strong>Saturday-Sunday:</strong> Closed
+              </span>
             </li>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>Email: Info@steadfastints.com</span>
+              <span>
+                <strong>Email:</strong> Info@steadfastints.com
+              </span>
             </li>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>US Office Phone 1: +1 929-567-2541</span>
+              <span>
+                <strong>US Office Phone 1:</strong> +1 929-567-2541
+              </span>
             </li>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>US Office Phone 2: +1 929-567-2540</span>
+              <span>
+                <strong>US Office Phone 2:</strong> +1 929-567-2540
+              </span>
             </li>
             <li>
               <FaAngleDoubleRight className="openningIcon" />
-              <span>Bangladesh Office Phone: +880 132-540-9732</span>
+              <span>
+                <strong>Bangladesh Office Phone:</strong> +880 132-540-9732
+              </span>
             </li>
           </ul>
+          <div className="follow">
+            <h3>Stay Connected!</h3>
+            <ul>
+              <li>
+                <Link to="https://www.linkedin.com/company/steadfastllc">
+                  <FaLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link to="https://www.facebook.com/Steadfast-International-Services-109044021870409/">
+                  <FaFacebookSquare />
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact">
+                  <FaMailBulk />
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact">
+                  <BsTelephoneFill />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="follow">
-        <h3>Stay Connected!</h3>
-        <ul>
-          <li>
-            <Link to="https://www.linkedin.com/company/steadfastllc">
-              <FaLinkedin />
-            </Link>
-          </li>
-          <li>
-            <Link to="https://www.facebook.com/Steadfast-International-Services-109044021870409/">
-              <FaFacebookSquare />
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">
-              <FaMailBulk />
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">
-              <BsTelephoneFill />
-            </Link>
-          </li>
-        </ul>
       </div>
       <div className="copyright">
         <span>
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,66 @@
+import "./contact.css";
+import Flag from "react-world-flags";
+import Footer from "../../components/footer/Footer";
+import { Link } from "react-router-dom";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-container">
+      <div className="heading-container">
+        <div className="heading-wrapper">
+          <p>
+            We’re a knowledgeable team and understand how important it is to
+            find the right fit for both companies and people! So, whether you’re
+            looking for your next big job, or searching for the perfect addition
+            to your team. We’ve got thousands of openings throughout the U.S.
+            both remote/work from home and in-person/on-site. We have huge range
+            of Hybrid job openings as well.
+          </p>
+          <span>
+            For more information on our services or any type of inquiries, feel
+            free to contact us.
+          </span>
+        </div>
+      </div>
+      <div className="country-icons">
+        <div className="country-icon">
+          <Flag code="us" height="100" />
+          <span>+1 929-567-2541</span>
+          <span>+1 929-567-2540</span>
+          <span>
+            733 3rd Ave, Floor 16 #1029, New York, NY 10017, United States{" "}
+          </span>
+        </div>
+        <div className="country-icon">
+          <Flag code="bd" height="115" />
+          <span>+880 132-540-9732</span>
+          <span>Dhaka Uddan, Adabor, Mohammadpur, Dhaka 1207, Bangladesh</span>
+        </div>
+      </div>
+      <div className="form-container">
+        <h1>Contact us</h1>
+        <form action=""></form>
+        {/* <label htmlFor="name">Name</label> */}
+        <input type="text" name="name" id="name" placeholder="Name*" />
+        {/* <label htmlFor="email">Email</label> */}
+        <input type="text" name="email" id="email" placeholder="Email*" />
+        {/* <label htmlFor="phone">Phone</label> */}
+        <input type="text" name="phone" id="phone" placeholder="Phone*" />
+        <textarea name="help" id="help" cols="30" rows="10">
+          How can we help?*
+        </textarea>
+        <div className="policy">
+          <label htmlFor="policy">Privacy Policy *</label>
+          <input type="checkbox" name="policy" id="policy" />
+          <span>
+            I consent to having this website store my submitted information so
+            Steadfast can respond to my inquiry.
+          </span>
+        </div>
+        <button className="submit">CONTACT DETAILS</button>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Contact;
